b154699435ca20f417f03d9b3fa45fdd24eddcbb
Mel <melissapixell@gmail.com>
2026-02-19 14:19:22 +0300

    build: remove extensions

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -6,9 +6,6 @@ ARG PHP_VERSION=8.2
 # Используем аргумент в названии образа
 FROM php:${PHP_VERSION}-cli
 
-# Устанавливаем полезные расширения
-RUN docker-php-ext-install pdo pdo_mysql
-
 WORKDIR /app
 
 EXPOSE 8000
